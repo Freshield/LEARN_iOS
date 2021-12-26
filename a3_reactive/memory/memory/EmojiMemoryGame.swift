5218fb322065e093ec43455fc8ac231101bb8b68
freshield <yangyufresh@163.com>
2021-12-26 20:51:28 +0800

    chp3 emoji

---
+++ a3_reactive/memory/memory/EmojiMemoryGame.swift
@@ -0,0 +1,30 @@
+    //
+//  EmojiMemoryGame.swift
+//  memory
+//
+//  Created by Yang Yu on 2021/5/7.
+//
+
+import SwiftUI
+
+
+class EmojiMemoryGame: ObservableObject {
+    @Published private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis = ["👻","🎃","🕷"]
+        return MemoryGame<String>(numberOfPairsOfCards: emojis.count, CardContentFactory: {pairIndex in emojis[pairIndex]})
+    }
+    
+    //MARK: - Access to the Model
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        model.cards
+    }
+    
+    func choose(card: MemoryGame<String>.Card) {
+        model.choose(card: card)
+    }
+    
+}
+
