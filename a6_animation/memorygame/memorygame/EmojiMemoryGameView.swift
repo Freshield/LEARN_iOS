857386d65aac6492e218dd2fcb7a0766af89a1a9
freshield <yangyufresh@163.com>
2022-01-16 20:53:52 +0800

    chp6 grid

--- a6_animation/memorygame/memorygame/EmojiMemoryGameView.swift
+++ a6_animation/memorygame/memorygame/EmojiMemoryGameView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  EmojiMemoryGameView.swift
 //  memorygame
 //
 //  Created by Yang Yu on 2021/6/13.
@@ -8,14 +8,57 @@
 import SwiftUI
 
 struct EmojiMemoryGameView: View {
+    @ObservedObject var viewModel: EmojiMemoryGame
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Grid(viewModel.cards) { card in
+            CardView(card: card).onTapGesture {
+                viewModel.choose(card: card)
+            }
+            .padding(5)
+        }
+        .padding()
+        .foregroundColor(Color.orange)
     }
 }
 
+
+struct CardView: View {
+    var card: MemoryGame<String>.Card
+    
+    var body: some View {
+        GeometryReader { geometry in
+            if card.isFaceUp || !card.isMatched {
+                ZStack {
+                    Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(110-90), clockwise: true)
+                        .padding(5).opacity(0.4)
+                    Text(card.content)
+                        .font(Font.system(size: fontSize(geometry.size)))
+                }
+                .cardify(isFaceUp: card.isFaceUp)
+            }
+        }
+    }
+    
+    private func fontSize(_ size: CGSize) -> CGFloat {
+        min(size.height, size.width) * 0.75
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        EmojiMemoryGameView()
+        EmojiMemoryGameView(viewModel: EmojiMemoryGame())
     }
 }
--- a6_animation/memorygame/memorygame/Grid.swift
+++ a6_animation/memorygame/memorygame/Grid.swift
@@ -5,4 +5,27 @@
 //  Created by Yang Yu on 2021/6/13.
 //
 
-import Foundation
+import SwiftUI
+
+struct Grid<Item, ItemView>: View where Item: Identifiable, ItemView: View {
+    private var items: [Item]
+    private var viewForItem: (Item) -> ItemView
+    
+    init(_ items: [Item], viewForItem: @escaping (Item) -> ItemView) {
+        self.items = items
+        self.viewForItem = viewForItem
+    }
+    
+    var body: some View {
+        GeometryReader { geometry in
+            let layout = GridLayout(itemCount: self.items.count, in: geometry.size)
+            ForEach(items) { item in
+                let index = self.items.firstIndex(matching: item)!
+                viewForItem(item)
+                    .frame(width: layout.itemSize.width, height: layout.itemSize.height)
+                    .position(layout.location(ofItemAt: index))
+            }
+        }
+    }
+    
+}
